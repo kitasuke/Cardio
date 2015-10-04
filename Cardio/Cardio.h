6292658a8048e77f1b87877246cd8094a965c48d
kitasuke <yusuke2759@gmail.com>
2015-10-04 17:18:06 +0900

    Initial Commit

---
+++ Cardio/Cardio.h
@@ -0,0 +1,19 @@
+//
+//  Cardio.h
+//  Cardio
+//
+//  Created by Yusuke Kita on 10/4/15.
+//  Copyright © 2015 kitasuke. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Cardio.
+FOUNDATION_EXPORT double CardioVersionNumber;
+
+//! Project version string for Cardio.
+FOUNDATION_EXPORT const unsigned char CardioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cardio/PublicHeader.h>
+
+
